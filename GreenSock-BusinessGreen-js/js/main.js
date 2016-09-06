a128970591085bd3ed28d8605e4183ac0d4dddb1
Davida Gaffney <gdavida@gmail.com>
2016-09-06 15:17:44 -0500

    gears rotating and morphing in test page

--- GreenSock-BusinessGreen-js/js/main.js
+++ GreenSock-BusinessGreen-js/js/main.js
@@ -3,9 +3,56 @@ $(function() {
   
   // Init ScrollMagic Controller
   var scrollMagicController = new ScrollMagic(),
-  triangle = document.getElementById("triangle");
+  triangle1 = document.getElementById("triangle1");
+
+  var tween1 = TweenLite.to(triangle1, 1, {morphSVG:"#square1"});
+      TweenMax.to('.gearItem', 5, {rotation:-360, transformOrigin: '53.3% 54.7%', ease:Linear.easeNone, repeat:-1});
+  
+
+  // Create the Scene and trigger when visible
+  var scene = new ScrollScene({
+    triggerElement: '#morph1',
+    offset: 150 /* offset the trigger 150px below #scene's top */
+  })
+  .setTween(tween1)
+  .addTo(scrollMagicController)
+  
+  // Add debug indicators fixed on right side
+   scene.addIndicators();
+  
+});
+
+// When the DOM is ready - MORPH1 triangle to hex
+$(function() {
+  
+  // Init ScrollMagic Controller
+  var scrollMagicController = new ScrollMagic(),
+  triangle3 = document.getElementById("triangle2");
   // Create Animation for 0.5s
-  var tween1 = TweenLite.to(triangle, 1, {morphSVG:"#hexagon"})
+  var tween1 = TweenLite.to(triangle2, 1, {morphSVG:"#square2"})
+  
+  
+
+  // Create the Scene and trigger when visible
+  var scene = new ScrollScene({
+    triggerElement: '#morph1',
+    offset: 150 /* offset the trigger 150px below #scene's top */
+  })
+  .setTween(tween1)
+  .addTo(scrollMagicController)
+  
+  // Add debug indicators fixed on right side
+   scene.addIndicators();
+  
+});
+// When the DOM is ready - MORPH1 triangle to hex
+$(function() {
+  
+  // Init ScrollMagic Controller
+  var scrollMagicController = new ScrollMagic(),
+  triangle2 = document.getElementById("triangle3");
+  // Create Animation for 0.5s
+  var tween1 = TweenLite.to(triangle3, 1, {morphSVG:"#square3"})
   
   
 
@@ -24,14 +71,77 @@ $(function() {
 
 
 
-// When the DOM is ready - MORPH3 almost gears to gears
+
+
+
+
+
+
+
+
+// 
+// 
+// 
+// 
+// v v v v v MORPH 2 v v v v v
+// 
+
+
+// When the DOM is ready - MORPH1 triangle to hex
 $(function() {
   
   // Init ScrollMagic Controller
   var scrollMagicController = new ScrollMagic(),
-  triangle = document.getElementById("triangle");
+  square1 = document.getElementById("square1");
   // Create Animation for 0.5s
-  var tween1 = TweenLite.to(triangle, 1, {morphSVG:"#almostGear"})
+  var tween1 = TweenLite.to(triangle1, 1, {morphSVG:"#hexagon1"})
+  
+  
+
+  // Create the Scene and trigger when visible
+  var scene = new ScrollScene({
+    triggerElement: '#morph2',
+    offset: 150 /* offset the trigger 150px below #scene's top */
+  })
+  .setTween(tween1)
+  .addTo(scrollMagicController)
+  
+  // Add debug indicators fixed on right side
+   scene.addIndicators();
+  
+});
+
+// When the DOM is ready - MORPH1 triangle to hex
+$(function() {
+  
+  // Init ScrollMagic Controller
+  var scrollMagicController = new ScrollMagic(),
+ square2 = document.getElementById("square2");
+  // Create Animation for 0.5s
+  var tween1 = TweenLite.to(triangle2, 1, {morphSVG:"#hexagon2"})
+  
+  
+
+  // Create the Scene and trigger when visible
+  var scene = new ScrollScene({
+    triggerElement: '#morph2',
+    offset: 150 /* offset the trigger 150px below #scene's top */
+  })
+  .setTween(tween1)
+  .addTo(scrollMagicController)
+  
+  // Add debug indicators fixed on right side
+   scene.addIndicators();
+  
+});
+// When the DOM is ready - MORPH1 triangle to hex
+$(function() {
+  
+  // Init ScrollMagic Controller
+  var scrollMagicController = new ScrollMagic(),
+  square3 = document.getElementById("square3");
+  // Create Animation for 0.5s
+  var tween1 = TweenLite.to(triangle3, 1, {morphSVG:"#hexagon3"})
   
   
 
@@ -52,14 +162,27 @@ $(function() {
 
 
 
-// When the DOM is ready - MORPH3 almost gears to gears
+
+
+
+
+
+// 
+// 
+// 
+// 
+// v v v v v MORPH 3 v v v v v
+// 
+
+
+// When the DOM is ready - MORPH1 triangle to hex
 $(function() {
   
   // Init ScrollMagic Controller
   var scrollMagicController = new ScrollMagic(),
-  triangle = document.getElementById("triangle");
+  hexagon1 = document.getElementById("hexagon1");
   // Create Animation for 0.5s
-  var tween1 = TweenLite.to(triangle, 1, {morphSVG:"#completeGear"})
+  var tween1 = TweenLite.to(triangle1, 1, {morphSVG:"#completeGear1"})
   
   
 
@@ -75,5 +198,56 @@ $(function() {
    scene.addIndicators();
   
 });
+
+// When the DOM is ready - MORPH1 triangle to hex
+$(function() {
+  
+  // Init ScrollMagic Controller
+  var scrollMagicController = new ScrollMagic(),
+ hexagon2 = document.getElementById("hexagon2");
+  // Create Animation for 0.5s
+  var tween1 = TweenLite.to(triangle2, 1, {morphSVG:"#completeGear2"})
+  
+  
+
+  // Create the Scene and trigger when visible
+  var scene = new ScrollScene({
+    triggerElement: '#morph3',
+    offset: 150 /* offset the trigger 150px below #scene's top */
+  })
+  .setTween(tween1)
+  .addTo(scrollMagicController)
+  
+  // Add debug indicators fixed on right side
+   scene.addIndicators();
+  
+});
+// When the DOM is ready - MORPH1 triangle to hex
+$(function() {
+  
+  // Init ScrollMagic Controller
+  var scrollMagicController = new ScrollMagic(),
+  hexagon3 = document.getElementById("hexagon3");
+  // Create Animation for 0.5s
+  var tween1 = TweenLite.to(triangle3, 1, {morphSVG:"#completeGear3"})
+  
+  
+
+  // Create the Scene and trigger when visible
+  var scene = new ScrollScene({
+    triggerElement: '#morph3',
+    offset: 150 /* offset the trigger 150px below #scene's top */
+  })
+  .setTween(tween1)
+  .addTo(scrollMagicController)
+  
+  // Add debug indicators fixed on right side
+   scene.addIndicators();
+  
+});
+
+
+
+
 
 
